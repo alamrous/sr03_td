8e0731af9f04849af166136e43448935e95e36ab
Aghiles <lamrous.aghiles@gmail.com>
2017-03-12 15:13:17 +0100

    td01 init

--- repo/td01/defobj.h
+++
@@ -1,9 +0,0 @@
- 
-struct obj
-{
-	char id[12];
-	char description[24];
-	int ii;
-	int jj;
-	double dd;
-};
--- repo/td01/iniobj.h
+++
@@ -1,9 +0,0 @@
-#include "defobj.h"
-#define tablen n
-
-	struct obj table[3] = {
-	{"ident_o1", "description_o1", 11, 12, 10.2345},{
-  "ident_o2", "description_o2", 21, 22, 20.2345},{
-  "ident_o3", "description_o3", 31, 32, 30.2345}};
-
-
